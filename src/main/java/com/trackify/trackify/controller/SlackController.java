2542afe243ed4138f2b78ab1e5e243f023c821e7
Amélia Alice <alice.cardoso10@gmail.com>
2025-10-26 18:43:39 -0300

    fix: use jakarta-servlet for Spring Boot 3 compatibility
    - Change bolt-servlet to bolt-jakarta-servlet dependency
    - Use SlackAppServlet from jakarta_servlet package
    - Simplify to @WebServlet approach (standard Slack SDK pattern)
    - Fixes compilation errors with Spring Boot 3

--- src/main/java/com/trackify/trackify/controller/SlackController.java
+++ src/main/java/com/trackify/trackify/controller/SlackController.java
@@ -1,31 +1,13 @@
 package com.trackify.trackify.controller;
 
 import com.slack.api.bolt.App;
-import com.slack.api.bolt.servlet.SlackAppServlet;
+import com.slack.api.bolt.jakarta_servlet.SlackAppServlet;
 import jakarta.servlet.annotation.WebServlet;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Slf4j
-@Configuration
-public class SlackController {
+@WebServlet("/slack/events")
+public class SlackController extends SlackAppServlet {
 
-    @Autowired
-    private App slackApp;
-
-    @Bean
-    public ServletRegistrationBean<SlackAppServlet> slackServletRegistration() {
-        log.info("Registering Slack servlet at /slack/events");
-
-        SlackAppServlet servlet = new SlackAppServlet(slackApp);
-        ServletRegistrationBean<SlackAppServlet> registration =
-            new ServletRegistrationBean<>(servlet, "/slack/events");
-
-        registration.setName("SlackAppServlet");
-
-        return registration;
+    public SlackController(App app) {
+        super(app);
     }
 }
